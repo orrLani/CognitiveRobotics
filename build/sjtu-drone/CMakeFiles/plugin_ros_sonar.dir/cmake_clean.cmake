694ba002a96459dfdf76a38c671f35853c41a4a9
orr <olaniado@campus.technion.ac.il>
2022-05-03 23:11:22 +0300

    add_drone

---
+++ build/sjtu-drone/CMakeFiles/plugin_ros_sonar.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "/home/orr/CognitiveRobotics/src/sjtu-drone/plugins/libplugin_ros_sonar.pdb"
+  "/home/orr/CognitiveRobotics/src/sjtu-drone/plugins/libplugin_ros_sonar.so"
+  "CMakeFiles/plugin_ros_sonar.dir/src/plugin_ros_sonar.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/plugin_ros_sonar.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/sjtu-drone/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/orr/CognitiveRobotics/src/sjtu-drone
+# Build directory: /home/orr/CognitiveRobotics/build/sjtu-drone
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
